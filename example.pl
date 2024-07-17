68caa6184d6f949f8164d4da9031f25c476ad0cb
KrisPersson <112572136+KrisPersson@users.noreply.github.com>
2024-07-17 12:19:06 +0200

    more practice

--- example.pl
+++ example.pl
@@ -44,3 +44,36 @@ customer(sally, smith, 120.55).
 get_customer_balance(FName, LName) :-
 	customer(FName, LName, Bal),
 	format("~w ~w's balance is $~2f", [FName, LName, Bal]).
+
+double_digit(X, Y) :-
+	Y is X * 2.
+
+say_hi :- 
+    write('What is your name?'),
+    read(X),
+    format('Hi ~w', [X]).
+
+fav_char :-
+    write('What is your favorite character?'),
+    get(X),
+    format('The ASCII value ~w is ', [X]),
+    put(X).
+
+
+write_to_file(Filename, TextBody) :-
+    open(Filename, write, Stream),
+    write(Stream, TextBody), nl,
+    close(Stream).
+
+read_file(Filename) :-
+    open(Filename, read, Stream),
+    get_char(Stream, Char1),
+    process_stream(Char1, Stream),
+    close(Stream).
+
+process_stream(end_of_file, _) :- !.
+
+process_stream(Char, Stream) :-
+    write(Char),
+    get_char(Stream, Char2),
+    process_stream(Char2, Stream).
